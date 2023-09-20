9daa9b04d4c62971e715d21365f25d2c403a0bb0
Kostiantyn <slimshady00713@gmail.com>
2023-09-20 08:43:18 +0300

    Created components: Header, Drawer, Card.

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="d-flex justify-between align-center p-40">
+      <div className="headerLeft d-flex align-center">
+        <img width={40} height={40} src="/img/logo.png" alt="logo" />
+        <div>
+          <h3 className="text-uppercase">Sneakers React</h3>
+          <p className="opacity-5">The best sneakers store</p>
+        </div>
+      </div>
+      <ul className="d-flex">
+        <li className="mr-30">
+          <img width={18} height={18} src="/img/cart.svg" alt="cart" />
+          <span>1205 usd</span>
+        </li>
+        <li>
+          <img
+            className="mr-30"
+            width={18}
+            height={18}
+            src="/img/user.svg"
+            alt="user"
+          />
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
